6dc7693347485fb19655f821f44dd30e05a6202e
nikitasingh0906 <nikitasingh8590@gmail.com>
2023-11-16 15:50:40 +0530

    abc

--- src/App.js
+++ src/App.js
@@ -3,29 +3,29 @@ import './App.css';
 import Navbar1 from './components/Navbar1';
 import Textform from './components/Textform';
 import { useState } from 'react';
-let name="Nikita";
+let name = "Nikita";
 function App() {
-const[mode,setMode]=useState('light');//dark mode is enable or not
- const toggleMode =()=>{
-  if(mode==='light'){
-  setMode('dark');
-  document.body.style.backgroundColor='#042743';
+  const [mode, setMode] = useState('light');//dark mode is enable or not
+  const toggleMode = () => {
+    if (mode === 'light') {
+      setMode('dark');
+      document.body.style.backgroundColor = '#042743';
+    }
+    else {
+      setMode('light'); // abcgcgsdcjsdojoper
+      document.body.style.backgroundColor = 'white';
+    }
   }
-  else{
-    setMode('light'); 
-    document.body.style.backgroundColor='white';
-  }
-}
   return (
     <>
-    <div className="setProperty">
-    <Navbar1 title="Textutils" mode={mode} toggleMode={toggleMode}/>
-   
-    <div className="container">
-    <Textform heading="Enter the text to analyze"  mode={mode}/>
-    </div>
-    </div>
-  </>
+      <div className="setProperty">
+        <Navbar1 title="Textutils" mode={mode} toggleMode={toggleMode} />
+
+        <div className="container">
+          <Textform heading="Enter the text to analyze" mode={mode} />
+        </div>
+      </div>
+    </>
   );
 }
 
